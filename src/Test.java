e2bf4de3e43c1b69f9ec1903bc13b5366326d4ee
Yeshaswini.m <Yeshaswini.m@192.168.1.5>
2023-02-15 22:13:51 +0530

    First Commit

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+		
+		if(hungry=="yes")
+			System.out.println("Eat something");
+		
+		else
+		{
+				System.out.println("Don't eat");
+			}
+		}
+	}
+
+
+
